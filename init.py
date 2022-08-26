2bfd4423ffd386eb4b08286e677439c229d2a6ab
Anthony Bui <anthonybui@gmail.com>
2022-08-25 23:09:50 -0700

    :wrench: Haphazardly tear out all audio code so teensy canhandle

--- init.py
+++ init.py
@@ -1,30 +1,14 @@
-import pygame
-from audio_control import Audio
 from dmx_control import Lights
 from trigger_control import Trigger
 import time
 
 def init():
-    # setup the audio class to pass into the trigger class
-    audio = Audio()
     lights = Lights()
+
     try:
-        # passing in audio into the trigger
-        trigger = Trigger(audio, lights)
+        trigger = Trigger(lights)
     except Exception as err:
         print(f"Problem starting trigger: {err}")
-
-#### this is simply here as a test
-# def cycle_play(audio):
-#     childMusic = "TheParting.wav"
-#     beastMusic = "SCP-x3x.wav"
-#     print(beastMusic)
-#     print(childMusic)
-#     while True:
-#         audio.play("SCP-x3x.wav")
-#         time.sleep(10)
-#         audio.play("TheParting.wav")
-#         time.sleep(10)
 
 print("Starting ~/Fearbeast/init.py")
 init()
--- trigger_control.py
+++ trigger_control.py
@@ -1,15 +1,11 @@
-import RPi.GPIO as GPIO
 import threading
 import time
 import serial
+
 class Trigger:
-    def __init__(self, audio, lights):
-        self.audio = audio
+    def __init__(self, lights):
         self.lights = lights
 
-        self.activateAudio = "TheParting.wav"
-        self.deactivateAudio = "SCP-x3x.wav"
-        
         self.ard = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         self.ard.reset_input_buffer()
 
@@ -19,40 +15,18 @@ class Trigger:
     
     def listen(self):
         print("starting trigger listen")
-        self.audio.play(self.activateAudio)
-        # "active" boolean used to gate the trigger
-        active = False
+
         while True:
             if self.ard.in_waiting > 0:
                 line = self.ard.readline().decode('utf-8').rstrip()
                 print(line)
                 if line == "ACTIVATED":
                     if not active:
-                        # print("Button was pushed!")
                         active = True
-                        self.audio.play(self.activateAudio)
                         self.lights.light_fade(True)
                         time.sleep(1)
                 if line == "DEACTIVATED":
                     if active:
-                        # print("Button was released")
                         active = False
-                        self.audio.play(self.deactivateAudio)
                         self.lights.light_fade(False)
-                        # self.lights.deactivate()
                         time.sleep(1)
-                    
-            # time.sleep(10)
-            # time.sleep(10)
-
-            # events = pygame.event.get()
-            # for event in events:
-            #     print(f"events {event}")
-            #     if event.type == pygame.KEYDOWN:
-            #         if event.key == pygame.K_UP:
-            #             print("space down")
-            #             self.audio.play("TheParting.wav")
-            #     if event.type == pygame.KEYUP:
-            #         if event.key == pygame.K_UP:
-            #             print("space up")
-            #             self.audio.play("SCP-x3x.wav")
